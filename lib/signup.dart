522ca9c6c33fe83d2de4b3455fbfb5ceabbe93c3
Audrey Gorter <algorter16@gmail.com>
2023-10-08 14:07:27 -0500

    Login and Create Account UI

---
+++ lib/signup.dart
@@ -0,0 +1,166 @@
+import 'package:campus_compass/login.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading:
+            // back button ** Change it to go straight back to home
+            IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: SizedBox(
+            // TODO: Fix keyboard covering input fields
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Create an Account, It's Free",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        SizedBox(height: 20)
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          makeInput(label: "First Name"),
+                          makeInput(label: "Last Name"),
+                          makeInput(label: "Email"),
+                          makeInput(label: "Password", obscureText: true),
+                          makeInput(
+                              label: "Confirm Password", obscureText: true),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 3, left: 3),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {
+                            // TODO: Navigate back to login or home with map?
+                          },
+                          color: Colors.lightBlue[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: const Text("Sign Up",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                                color: Colors.white,
+                              )),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Already have an account?  "),
+                        GestureDetector(
+                          onTap: () {
+                            // Navigate to login page
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginPage()),
+                            );
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      )),
+    );
+  }
+}
+
+Widget makeInput({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(
+        height: 3,
+      ),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          border:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+        ),
+      ),
+      const SizedBox(
+        height: 15,
+      )
+    ],
+  );
+}
