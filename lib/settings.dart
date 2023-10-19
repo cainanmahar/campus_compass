e79f4fa35f3d8f3d1f154edf5416ee3298223002
Audrey Gorter <algorter16@gmail.com>
2023-10-19 17:55:18 -0500

    Added change email button

--- lib/settings.dart
+++ lib/settings.dart
@@ -16,35 +16,50 @@ class _SettingsPageState extends State<SettingsPage> {
         backgroundColor: Colors.lightBlue[800],
       ),
       body: SafeArea(
-          child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Account Settings',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                  ),
-                  SizedBox(height: 20),
-                ],
-              ))),
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Account Settings',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+              const SizedBox(height: 40),
 
-      /* ListView(
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () {}, //_changeEmailDialog,
-            child: const Text('Change Email'),
+              // Change Name Button
+
+              // Change Email button
+              MaterialButton(
+                minWidth: double.infinity,
+                height: 60,
+                onPressed: () {
+                  // TODO create dialog box to change email
+                },
+                color: Colors.orange,
+                shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(40)),
+                child: const Text(
+                  'Change Email',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+
+              // Change Password Button
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {}, //_changePasswordDialog,
-            child: const Text('Change Password'),
-          ),
-          // Other settings options can be added here
-        ],
-      ),*/
+        ),
+      ),
     );
   }
 }
