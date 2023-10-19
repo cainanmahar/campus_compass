f50d6c59715f32fbaf4308ac5b675e1f027a92c9
peter_cerna <petercerna@gmail.com>
2023-10-19 16:56:03 -0500

    test_data.dart staged and dummy test data ready with 6 course.

--- lib/test_data.dart
+++ lib/test_data.dart
@@ -3,9 +3,9 @@ import 'package:campus_compass/add_classes.dart';
 List<Course> dummyCourses = [
   Course(courseNumber: '1436', className: 'Programming Fundamentals I', professorName: 'Audrey Gorter', building: 'AB1' , roomNumber: '204'),
   Course(courseNumber: '1437', className: 'Programming Fundamentals II', professorName: 'Audrey Gorter', building: 'AB1' , roomNumber: '206'),
-  Course(courseNumber: '1436', className: 'Special Topics', professorName: 'Peter Cerna', building: 'AB1' , roomNumber: '204'),
-  Course(courseNumber: '1436', className: 'Data Structures', professorName: 'Audrey Gorter', building: 'AB1' , roomNumber: '204'),
-  Course(courseNumber: '1437', className: 'Data Mining', professorName: 'Hyuk Cho', building: 'AB1' , roomNumber: '206'),
-  Course(courseNumber: '1436', className: 'Software Engineering', professorName: 'Cainan Mahar', building: 'AB1' , roomNumber: '206'),
+  Course(courseNumber: '2347', className: 'Special Topics', professorName: 'Peter Cerna', building: 'AB1' , roomNumber: '204'),
+  Course(courseNumber: '3319', className: 'Data Structures', professorName: 'Audrey Gorter', building: 'AB1' , roomNumber: '204'),
+  Course(courseNumber: '4314', className: 'Data Mining', professorName: 'Hyuk Cho', building: 'AB1' , roomNumber: '206'),
+  Course(courseNumber: '4319', className: 'Software Engineering', professorName: 'Cainan Mahar', building: 'AB1' , roomNumber: '206'),
 
 ];
